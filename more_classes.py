64de4c3ad94ebd692c99b8dee8a9d53327853a53
awilmes <85970174+awilmes@users.noreply.github.com>
2021-06-23 13:29:26 -0500

    Update more_classes.py

--- more_classes.py
+++ more_classes.py
@@ -1,8 +1,11 @@
 class Rectangle:
+    recs = []
+    
     def __init__(self, l, w):
         self.length = l
         self.width = w
         print("Created!")
+        self.recs.append(self.length, self.width)
     def calculatePerimeter(self):
         return (self.length*2)+(self.width*2)
     def changeSize(self):
@@ -13,9 +16,11 @@ class Rectangle:
         
         
 class Square:
+    sqs = []
     def __init__(self, l):
         self.length = l
         print("Created!")
+        self.sqs.append(self.length)
     def calculatePerimeter(self):
         return self.length * 4
     def changeSize(self):
